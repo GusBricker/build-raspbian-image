f0c5e00ee7b898c69bd510833932788cb3cb0c3a
GusBricker <chris@lapa.com.au>
2015-10-06 23:10:58 +1100

    chroot: functions: added several helper functions to be used later.
    Signed-off-by: GusBricker <chris@lapa.com.au>

--- delivery/functions.sh
+++ delivery/functions.sh
@@ -1,5 +1,28 @@
 #!/bin/bash
 
+WORKSPACE_DIR="/opt/build"
+
+function RMSafe()
+{
+    rm --one-file-system --preserve-root $@
+}
+
+function GetWorkspace()
+{
+    local name="$@"
+
+    mkdir -p "${WORKSPACE_DIR}"
+
+    echo "${WORKSPACE_DIR}/${name}"
+}
+
+function ReleaseWorkspace()
+{
+    local name="$@"
+
+    RMSafe -r "${WORKSPACE_DIR}/${name}"
+}
+
 function BannerEcho()
 {
     echo "------------------------------------------------------------------"
@@ -8,6 +31,18 @@ function BannerEcho()
         echo "    $arg"
     done
     echo "------------------------------------------------------------------"
+}
+
+function PipInstall()
+{
+    local to_install="$1"
+    if [ -n "$(pip install ${to_install})" ]
+    then
+        RMSafe -r $HOME/.cache/pip
+        pip install ${to_install} || return 1
+    fi
+
+    return 0
 }
 
 function AptUpdate()
@@ -52,4 +87,27 @@ function DirectoryOrderedExecute()
         echo "Applying ${cfgn}..."
         source ${cfg}
     done
+}
+
+function CheckDownload()
+{
+    local uri="$1"
+    local output_file="$2"
+    local wget_args="--span-hosts --level=10 --no-check-certificate --restrict-file-names=nocontrol --retry-connrefused --content-disposition"
+
+    if [ -f "${output_file}" ]
+    then
+        return 0
+    fi
+
+    wget ${wget_args} "${uri}" -O "${output_file}"
+}
+
+function ExtractTar()
+{
+    local src_tar="$1"
+    local src_dir="$2"
+
+    mkdir -p ${src_dir}
+    tar -xaf "${src_tar}" -C "${src_dir}" --strip-components=1
 }
